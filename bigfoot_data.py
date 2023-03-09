9e37ff4cdf00555d1fedc313c11b22ae1ff0f7eb
Ryan Huddleston <rhuddlest@gmail.com>
2023-03-08 20:00:37 -0500

    bigfoot_data.py

--- bigfoot_data.py
+++ bigfoot_data.py
@@ -3,35 +3,32 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-
-# Having issues with path right now, can't seem to change path in settings from 'Users/rjhud' this is a work around I guess. 
-
-os.chdir('/Users/rjhud/Pluralsight/Projects/bigfoot_sightings/data_main')
-
-# I'm using this as a test file kinda thing
-
-df = pd.read_csv('best_data.csv')
-df = df[0:5]
-df. drop(['observed','location_details'],axis=1,inplace=True)
-print(df)
 
 # This should return a pie chart 
 
-df =  pd.read_csv('best_data.csv')
- y = df['day_of_the_week'].value_counts(normalize=True) * 100
+df = pd.read_csv('data_main/merged_data.csv')
+y = df['day_of_the_week'].value_counts(normalize=True) * 100
 colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#8c564b", "#800000" ,"#FFFF00"]
-mylabels = ['Sunday,', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+mylabels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 plt.pie(y, labels = mylabels,autopct='%1.1f%%')
 myexplode = [0.2, 0, 0, 0]
 plt.title("On Which Day Are You Most Likely to\n" + "See a real-life Bigfoot?")
 plt.show()  
 
+#This should return a histogram
+#df = pd.read_csv('data_main/merged_data.csv')
+season_data = df['season'].value_counts().plot(kind='bar')
+plt.hist(colors2 = ['#F6FF43', '#FB8908', '#39FC00', '#0089FC'])
+label = 'Unknown'
+#df.drop(label='Unknown', inplace=True)
+plt.title("Sasquatch by Season")
+plt.ylabel("Number of Sightings")
+plt.ylim(0, 4000)   
+plt.show()
 
 
 
 
 
 
-plt.pie(, explode=explode, labels=labels colors=colors, shadow=True, startangle=140)
-labels = ['Sunday,', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
