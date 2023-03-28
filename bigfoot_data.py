61bd8eeed19a6fbc4c2172224f509c3bb0cd6198
Ryan Huddleston <rhuddlest@gmail.com>
2023-03-28 11:10:46 -0400

    adding cleaning code

--- bigfoot_data.py
+++ bigfoot_data.py
@@ -5,15 +5,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Here's where my I'm pulling my data from
-df = pd.read_csv('data_main/merged_data.csv')
+df = pd.read_csv('data_main/merged_data.csv').drop(columns=['count', 'observed','location_details', 'title', 'pressure', 'summary', 'uv_index', 'visibility', 'wind_bearing','wind_speed', 'temperature_mid', 'dew_point','humidity','cloud_cover','precip_intensity','precip_probability','precip_type', 'latitude', 'longitude', 'geohash'])
+
+#Cleaning process
+
+# This shows us our data frame minus some columns that affect how readable the information is. 
+
+df.head(10)
+
+df.tail(10)
+
+df['county'].describe()
+
+df['temperature_high'].mean()
+
+df['temperature_low'].mean()
+
 
 # This should return a pie chart that tells us the day of the week most spotters 
 
 y = df['day_of_the_week'].value_counts(normalize=True) * 100
 colors = ["#CCBFE9", "#B8E9E4", "#FEF3D8", "#FFD3CC", "#FFF8C2", "#CFF3CA" ,"#FFE5B8"]
 mylabels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-plt.pie(y, labels = mylabels,autopct='%1.1f%%', colors=colors)
-#myexplode = [0.2, 0, 0, 0]
+myexplode = [0.1, 0, 0, 0, 0, 0, 0.1]
+plt.pie(y, labels = mylabels,explode=myexplode, autopct='%1.1f%%', colors=colors)
 plt.title("On Which Day Are You Most Likely to\n" + "See a real-life Bigfoot?")
 plt.show()  
 
